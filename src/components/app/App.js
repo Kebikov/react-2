bf242beb1a178b91bbd3abf6aa75dda500cde83a
Gen <kebikovgen@gmail.com>
2022-10-04 23:24:38 +0300

    heroku

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,18 +1,10 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import decoration from '../../resources/img/vision.png';
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-
-const MyDiv = (props) => {
-    return (
-        <div style={{border: '2px solid ' + props.color}}>
-            {props.children}
-        </div>
-    )
-}
 
 
 class App extends Component {
@@ -28,17 +20,15 @@ class App extends Component {
         return (
             <div className="app">
                 <AppHeader/>
-                <MyDiv color={'green'}>
-                    <h1>Hello!</h1>
-                </MyDiv>
                 <main>
                     <RandomChar/>
                     <div className="char__content">
                         <CharList 
-                            onCharSelected={this.onCharSelected}/>
+                        onCharSelected={this.onCharSelected}
+                        charId={this.state.selectedChar}/>
                         <ErrorBoundary>
                             <CharInfo
-                                charId={this.state.selectedChar}/>
+                            charId={this.state.selectedChar}/>
                         </ErrorBoundary>
                     </div>
                     <img className="bg-decoration" src={decoration} alt="vision"/>
@@ -47,6 +37,8 @@ class App extends Component {
         )
     }
 }
+
+
 
 
 export default App;
